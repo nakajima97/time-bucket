5043ed099ad4df3f6223d10d771a40a5a6d7597d
nakajima97 <kituneudon97@gmail.com>
2024-09-11 08:53:55 +0900

    #2 biomeの実行

--- app/root.tsx
+++ app/root.tsx
@@ -1,3 +1,4 @@
+import { ColorSchemeScript, MantineProvider } from '@mantine/core';
 import {
 	Links,
 	Meta,
@@ -5,7 +6,6 @@ import {
 	Scripts,
 	ScrollRestoration,
 } from '@remix-run/react';
-import { ColorSchemeScript, MantineProvider } from '@mantine/core';
 import './tailwind.css';
 
 export function Layout({ children }: { children: React.ReactNode }) {
