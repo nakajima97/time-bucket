cb9785595ca3bdedfe542edf41d9f5a6d7f864b7
nakajima97 <kituneudon97@gmail.com>
2024-09-14 18:08:56 +0900

    #5 biomeの実行

--- app/components/Layout/index.stories.tsx
+++ app/components/Layout/index.stories.tsx
@@ -2,14 +2,14 @@ import type { StoryObj } from '@storybook/react';
 import { Layout } from './index';
 
 export default {
-  title: 'components/Layout',
-  component: Layout,
+	title: 'components/Layout',
+	component: Layout,
 };
 
 type Story = StoryObj<typeof Layout>;
 
 export const Default: Story = {
-  args: {
-    children: <div>Content</div>,
-  },
+	args: {
+		children: <div>Content</div>,
+	},
 };
--- app/components/QuestionBase/index.stories.tsx
+++ app/components/QuestionBase/index.stories.tsx
@@ -2,14 +2,14 @@ import type { StoryObj } from '@storybook/react';
 import { QuestionBase } from './index';
 
 export default {
-  title: 'components/QuestionBase',
-  component: QuestionBase,
+	title: 'components/QuestionBase',
+	component: QuestionBase,
 };
 
 type Story = StoryObj<typeof QuestionBase>;
 
 export const Default: Story = {
-  args: {
-    children: <div>Content</div>,
-  },
+	args: {
+		children: <div>Content</div>,
+	},
 };
