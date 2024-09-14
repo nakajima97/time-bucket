ba2ec6bad028943c12d19aa058034ca8c02d9ab2
nakajima97 <kituneudon97@gmail.com>
2024-09-14 17:26:39 +0900

    #5 ファイルの整理

--- app/routes/bucket.tsx
+++ app/routes/bucket.tsx
@@ -1,10 +1,19 @@
 import { Layout } from '@/components/Layout';
-import { Question } from '@/features/bucket/components/Question';
+import { BucketListContainer } from '@/features/bucket/components/BucketListContainer';
+import { TaskListContainer } from '@/features/bucket/components/TaskListContainer';
+import { Box, Flex } from '@mantine/core';
 
 const Index = () => {
 	return (
 		<Layout>
-			<Question />
+			<Flex style={{ gap: '16px', width: '100%', height: '100%' }}>
+			<Box style={{ width: '300px', flexShrink: 0 }}>
+				<TaskListContainer />
+			</Box>
+			<Box style={{ flexGrow: 1, minWidth: 0, overflowX: 'auto' }}>
+				<BucketListContainer />
+			</Box>
+		</Flex>
 		</Layout>
 	);
 };
