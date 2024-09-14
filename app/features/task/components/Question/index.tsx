bbf69383e1e5af95acd64fd4fa292c3f2251ee8a
nakajima97 <kituneudon97@gmail.com>
2024-09-14 11:13:03 +0900

    #1 タスクリストを作成

--- app/features/task/components/Question/index.tsx
+++ app/features/task/components/Question/index.tsx
@@ -1,11 +1,15 @@
 import { QuestionBase } from "@/components/QuestionBase"
-import { Button, Text } from "@mantine/core"
+import { Button, Flex, Input, Text } from "@mantine/core"
 import { TaskList } from "../TaskList"
 
 export const Question = () => {
   return (
     <QuestionBase>
-      <Text>あなたは現在何歳ですか？</Text>
+      <Text>〇歳までにやりたいことは何ですか？</Text>
+      <Flex style={{ gap: '10px' }}>
+        <Input type="text" placeholder="やりたいことを入力してください" />
+        <Button>追加</Button>
+      </Flex>
       <TaskList />
       <Button>次へ</Button>
     </QuestionBase>
