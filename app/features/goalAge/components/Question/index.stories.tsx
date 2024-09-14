1e80a5aa3f4078fa6dabb55cb3551e621ba27970
nakajima97 <kituneudon97@gmail.com>
2024-09-14 17:41:27 +0900

    #5 ストーリーの追加

---
+++ app/features/goalAge/components/Question/index.stories.tsx
@@ -0,0 +1,15 @@
+import type { StoryObj } from '@storybook/react';
+import { Question } from '.';
+
+export default {
+  title: 'Features/GoalAge/Components/Question',
+  component: Question,
+};
+
+type Story = StoryObj<typeof Question>;
+
+export const Default: Story = {
+  args: {
+    onClick: () => {}
+  },
+}
