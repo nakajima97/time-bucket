7a0f1ead5d36ef40d0639e963746b7ef6f1f197b
nakajima97 <kituneudon97@gmail.com>
2024-09-14 14:59:25 +0900

    #5 動作確認できていないがコードの作成

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,8 +2,10 @@ import type { StorybookConfig } from '@storybook/react-vite';
 
 const config: StorybookConfig = {
 	stories: [
-		'../stories/**/*.mdx',
-		'../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)',
+		// '../app/**/*.mdx',
+		'../app/**/*.stories.@(js|jsx|mjs|ts|tsx)',
+		"../stories/**/*.mdx",
+		"../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)",
 	],
 	addons: [
 		'@storybook/addon-onboarding',
---
+++ app/features/ageVerification/components/Question/story.stories.tsx
@@ -0,0 +1,10 @@
+import { Question } from ".";
+
+export default {
+  title: "Features/AgeVerification/Components/Question",
+  component: Question,
+};
+
+const Template = () => <Question />;
+
+const Default = Template.bind({});
