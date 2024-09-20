9ee1e7db8bc743bd988fb855e8ad661f9ce92fcb
Yudai Nakajima <kituneudon97@gmail.com>
2024-09-20 14:51:39 +0900

    #19 定数を使うようにした

--- app/features/startAge/components/QuestionContainer/index.tsx
+++ app/features/startAge/components/QuestionContainer/index.tsx
@@ -1,10 +1,11 @@
 import { useAgeInput } from '@/hooks/useAgeInput';
 import { useNavigate } from '@remix-run/react';
 import { Question } from '../Question';
+import { localStorageKeys } from '@/utils/constants/localStorageKeys';
 
 export const QuestionContainer = () => {
 	const { age, handleChange, setLocalStorageAge } = useAgeInput({
-		localStorageKey: 'startAge',
+		localStorageKey: localStorageKeys.startAge,
 	});
 
 	const navigate = useNavigate();
