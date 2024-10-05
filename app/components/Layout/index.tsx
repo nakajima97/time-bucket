ee9798772966376b5ddf78a8839637b6fa18cd1e
nakajima97 <kituneudon97@gmail.com>
2024-10-05 14:47:56 +0900

    #34 デザイン作成

--- app/components/Layout/index.tsx
+++ app/components/Layout/index.tsx
@@ -1,9 +1,36 @@
-import { Box } from '@mantine/core';
+import { AppShell, Box, Button, Flex, Text } from '@mantine/core';
 
 type Props = {
 	children: React.ReactNode;
 };
 
 export const Layout = ({ children }: Props) => {
-	return <Box style={{ width: '100dvw', height: '100dvh' }}>{children}</Box>;
+	return (
+		<AppShell
+			header={{ height: 60 }}
+			style={{
+				width: '100dvw',
+				height: '100dvh',
+				display: 'flex',
+				flexDirection: 'column',
+			}}
+		>
+			<AppShell.Header
+				style={{
+					backgroundColor: '#1976D2',
+					display: 'flex',
+					justifyContent: 'space-between',
+					alignItems: 'center',
+				}}
+			>
+				<Text size="lg" style={{ color: 'white' }}>
+					タイムバケットアプリ
+				</Text>
+				<Button>データクリア</Button>
+			</AppShell.Header>
+			<AppShell.Main style={{ flexGrow: 1, paddingY: '16px' }}>
+				{children}
+			</AppShell.Main>
+		</AppShell>
+	);
 };
